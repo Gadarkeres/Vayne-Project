1cce0f130080603a985e785cb11c5ff3a7cc20e7
Matheus Rafael <matheusrafael77@hotmail.com>
2023-09-26 00:21:04 -0300

    btn reset

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const btn_expand = document.querySelector("#expand-text")
 const text = document.querySelector("#text_hidden")
 const change_button = document.querySelector("#change-page")
 const initial_container = document.querySelector("#initial-container")
+const reset_button = document.querySelector("#reset_button")
 // main page
 
 const main_container = document.querySelector("#container")
@@ -15,12 +16,23 @@ btn_expand.addEventListener('click',  () => {
 })
 
 //mudar de pagina
-
 change_button.addEventListener('click', () => {
-    initial_container.classList.add("trocar_pagina")
+    if (initial_container.classList.contains('reset_page')) {
+        initial_container.classList.remove('reset_page');
+    }
+    initial_container.classList.add('trocar_pagina');
     setTimeout(() => {
-        main_container.style.display = "flex";
-    }, 850); 
-  
-})
+        main_container.style.display = 'flex';
+    }, 850);
+});
+// voltar pagina
+const resetInitialContainer = () => {
+    initial_container.classList.remove("trocar_pagina");
+    initial_container.classList.add("reset_page");
+    main_container.style.display = "none";
+}
+
+
+    
+reset_button.addEventListener('click', resetInitialContainer)
 
