ecb2be7a980d6f368372d9baffe3e8cc0f64b36f
Matheus Rafael <matheusrafael77@hotmail.com>
2023-09-27 16:52:08 -0300

    up

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+// pagina inicial
 const btn_expand = document.querySelector("#expand-text");
 const text = document.querySelector("#text_hidden");
 const change_button = document.querySelector("#change-page");
 const initial_container = document.querySelector("#initial-container");
 const reset_button = document.querySelector("#reset_button");
 const body = document.querySelector("#body")
-// main page
+
+// pagina principal
 const habilityQ = document.querySelector("#habilityQ")
 const habilityW = document.querySelector("#habilityW")
 const habilityE = document.querySelector("#habilityE")
@@ -14,7 +16,6 @@ const svg = document.querySelector("#svg")
 const svg2 = document.querySelector("#svg2")
 const svg3 = document.querySelector("#svg3")
 const svg4 = document.querySelector("#svg4")
-
 const main_container = document.querySelector("#container");
 
 // expandir texto
@@ -32,6 +33,7 @@ change_button.addEventListener("click", () => {
   setTimeout(() => {
     main_container.style.display = "flex";
     body.style.min_height = "100vh"
+    
   }, 800);
 });
 // voltar pagina
@@ -56,11 +58,13 @@ const toggleHability = (hability, svg) => {
   const isExpanded = hability.classList.contains('hability_expand');
   
   if (isExpanded) {
+    hability.scrolltop = 0
     hability.classList.remove('hability_expand');
     svg.classList.remove('svg_transform');
     svg.classList.add('svg_back');
-    svg.style.top('35px')
-    svg.style.right('30px')
+    svg.style.top ='35px'
+    svg.style.right ='30px'
+   
   } else {
     hability.classList.add('hability_expand');
     svg.classList.remove('svg_back');
